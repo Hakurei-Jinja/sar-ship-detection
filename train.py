f6f46c0ea2e36410ef8533c82624a065204ad248
KurisuNya <cuizhu1216@gmail.com>
2024-04-17 03:18:51 +0800

    fix: small fix

--- train.py
+++ train.py
@@ -2,22 +2,6 @@ from src.dataset import DatasetConvertor
 from src.nn.model import MyYOLO
 
 
-def convert_SSDD():
-    dataset_convertor = DatasetConvertor()
-    dataset_convertor.load_config(
-        "./datasets/SSDD/cfg/convertor.yaml", "./datasets/class.yaml"
-    )
-    dataset_convertor.convert()
-
-
-def convert_HRSID():
-    dataset_convertor = DatasetConvertor()
-    dataset_convertor.load_config(
-        "./datasets/HRSID_png/cfg/convertor.yaml", "./datasets/class.yaml"
-    )
-    dataset_convertor.convert()
-
-
 if __name__ == "__main__":
     model = MyYOLO("./models/cfg/yolov8_sar.yaml", verbose=True)
     result = model.train(
