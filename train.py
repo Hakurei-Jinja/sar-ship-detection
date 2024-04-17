9a5d9ac42ff39721325d56965a008a9d259cca2e
KurisuNya <cuizhu1216@gmail.com>
2024-04-17 14:29:02 +0800

    feat: add diff v8 models

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from src.nn.model import MyYOLO
 
 
 if __name__ == "__main__":
-    model = MyYOLO("./models/cfg/yolov8_sar.yaml", verbose=True)
+    model = MyYOLO("./models/cfg/yolov8.yaml", verbose=True)
     result = model.train(
         data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000
     )  # will stop training if no improvement in 100 epochs
