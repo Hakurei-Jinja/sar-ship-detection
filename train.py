9bcfde32c4b9f81169931ae381b628e9d9a7e466
KurisuNya <cuizhu1216@gmail.com>
2024-04-19 20:24:39 +0800

    feat: change sa layer

--- train.py
+++ train.py
@@ -2,31 +2,23 @@ from src.nn.model import MyYOLO
 
 
 if __name__ == "__main__":
-    model = MyYOLO("./models/cfg/yolov8.yaml", verbose=True)
-    result = model.train(
-        data="./datasets/SSDD/cfg/ssdd_all.yaml",
-        epochs=2000,
-        batch=-1,
-        optimizer="AdamW",
-    )  # will stop training if no improvement in 100 epochs
     model = MyYOLO("./models/cfg/yolov8_sa.yaml", verbose=True)
     result = model.train(
-        data="./datasets/SSDD/cfg/ssdd_all.yaml",
-        epochs=2000,
-        batch=-1,
-        optimizer="AdamW",
+        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000, batch=100
     )  # will stop training if no improvement in 100 epochs
-    model = MyYOLO("./models/cfg/yolov8_sppfcspc.yaml", verbose=True)
+    model = MyYOLO("./models/cfg/yolov8_sa_fix.yaml", verbose=True)
     result = model.train(
-        data="./datasets/SSDD/cfg/ssdd_all.yaml",
-        epochs=2000,
-        batch=-1,
-        optimizer="AdamW",
+        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000, batch=100
     )  # will stop training if no improvement in 100 epochs
-    model = MyYOLO("./models/cfg/yolov8_sa_sppfcspc.yaml", verbose=True)
+    model = MyYOLO("./models/cfg/yolov8_sppelan.yaml", verbose=True)
     result = model.train(
-        data="./datasets/SSDD/cfg/ssdd_all.yaml",
-        epochs=2000,
-        batch=-1,
-        optimizer="AdamW",
+        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000, batch=100
+    )  # will stop training if no improvement in 100 epochs
+    model = MyYOLO("./models/cfg/yolov8_sa_sppelan.yaml", verbose=True)
+    result = model.train(
+        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000, batch=100
+    )  # will stop training if no improvement in 100 epochs
+    model = MyYOLO("./models/cfg/yolov8_sa_fix_sppelan.yaml", verbose=True)
+    result = model.train(
+        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000, batch=100
     )  # will stop training if no improvement in 100 epochs
