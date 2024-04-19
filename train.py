c413ed933a85f6e473aa0f8cf67fb337e2db42ca
KurisuNya <cuizhu1216@gmail.com>
2024-04-19 12:58:08 +0800

    feat: change optimizer to AdamW

--- train.py
+++ train.py
@@ -4,5 +4,29 @@ from src.nn.model import MyYOLO
 if __name__ == "__main__":
     model = MyYOLO("./models/cfg/yolov8.yaml", verbose=True)
     result = model.train(
-        data="./datasets/SSDD/cfg/ssdd_all.yaml", epochs=2000
+        data="./datasets/SSDD/cfg/ssdd_all.yaml",
+        epochs=2000,
+        batch=-1,
+        optimizer="AdamW",
+    )  # will stop training if no improvement in 100 epochs
+    model = MyYOLO("./models/cfg/yolov8_sa.yaml", verbose=True)
+    result = model.train(
+        data="./datasets/SSDD/cfg/ssdd_all.yaml",
+        epochs=2000,
+        batch=-1,
+        optimizer="AdamW",
+    )  # will stop training if no improvement in 100 epochs
+    model = MyYOLO("./models/cfg/yolov8_sppfcspc.yaml", verbose=True)
+    result = model.train(
+        data="./datasets/SSDD/cfg/ssdd_all.yaml",
+        epochs=2000,
+        batch=-1,
+        optimizer="AdamW",
+    )  # will stop training if no improvement in 100 epochs
+    model = MyYOLO("./models/cfg/yolov8_sa_sppfcspc.yaml", verbose=True)
+    result = model.train(
+        data="./datasets/SSDD/cfg/ssdd_all.yaml",
+        epochs=2000,
+        batch=-1,
+        optimizer="AdamW",
     )  # will stop training if no improvement in 100 epochs
