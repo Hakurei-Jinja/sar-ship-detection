51db4bd1624b54cc45814e49696c584cdeec10c8
KurisuNya <cuizhu1216@gmail.com>
2024-04-28 13:36:20 +0800

    feat: add sc cfg

--- train.py
+++ train.py
@@ -163,10 +163,11 @@ if __name__ == "__main__":
             # "./models/cfg/detect/v8n-sa.yaml",
             # "./models/cfg/detect/v8n-dc.yaml",
             # "./models/cfg/detect/v8n-sa-dc.yaml",
-            "./models/cfg/detect/v8n-sh.yaml",
-            "./models/cfg/detect/v8n-sh-sa.yaml",
-            "./models/cfg/detect/v8n-sh-dc.yaml",
-            "./models/cfg/detect/v8n-sh-sa-dc.yaml",
+            # "./models/cfg/detect/v8n-sh.yaml",
+            # "./models/cfg/detect/v8n-sh-sa.yaml",
+            # "./models/cfg/detect/v8n-sh-dc.yaml",
+            # "./models/cfg/detect/v8n-sh-sa-dc.yaml",
+            "./models/cfg/detect/v8n-sc.yaml",
         ]
     )
     # ssdd_obb_trainer.train(
